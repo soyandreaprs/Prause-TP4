7fb8dab9a8d8909273e5d3ebb84ad78736803a5a
Andrea <soyandreaprs@gmail.com>
2026-01-15 11:58:32 -0300

    Implementación de controladores para las rutas

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,90 @@
+import UserDTO from "../dtos/user-dto.js";
+import { userService } from "../services/user-service.js";
+import { generateToken } from "../utils/user-util.js";
+
+class UserController {
+  constructor(service) {
+    this.service = service;
+  }
+
+  register = async (req, res, next) => {
+    try {
+      const user = await this.service.register(req.body);
+      res.status(201).json({
+        message: "Usuario registrado con éxito",
+        user: new UserDTO(user),
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  login = async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+      const user = await this.service.login(email, password);
+      const token = generateToken(user);
+      res.header("Authorization", `Bearer ${token}`);
+      res.status(200).json({
+        message: "Login realizado con éxito",
+        token,
+        user: new UserDTO(user),
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  update = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await this.service.update(parseInt(id), req.body);
+      res.json({
+        message: "Usuario actualizado con éxito",
+        user: new UserDTO(user),
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  delete = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await this.service.delete(parseInt(id));
+      res.json({ message: "Usuario eliminado con éxito" });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  findMe = async (req, res, next) => {
+    try {
+      const user = req.user;
+      res.json({ user: new UserDTO(user) });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  findById = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await this.service.findById(id);
+      res.json({ user: new UserDTO(user) });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  findAll = async (req, res, next) => {
+    try {
+      const users = await this.service.findAll();
+      res.json({ users: users.map((user) => new UserDTO(user)) });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export const userController = new UserController(userService);
