f4f12e1fa0092b7d72391578a0e57e1a0c2f7d32
Andrea <soyandreaprs@gmail.com>
2026-01-16 09:48:27 -0300

    Corrección de problemas de autorización

--- src/middlewares/auth-middlewares.js
+++ src/middlewares/auth-middlewares.js
@@ -2,13 +2,26 @@ import jwt from "jsonwebtoken";
 import config from "../config/config.js";
 import CustomError from "../utils/custom-error.js";
 
-export const authMiddleware = (req, res, next) => {
+export const authenticationMiddleware = (req, res, next) => {
   try {
     const token = req.header("Authorization");
     if (!token) throw new CustomError("Token inválido", 401);
     const cleanToken = token.replace("Bearer ", "");
     const payload = jwt.verify(cleanToken, config.SECRET_KEY);
     req.user = payload;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const authorizationMiddleware = (req, res, next) => {
+  try {
+    const paramId = req.params.id;
+    const tokenUserId = req.user.id;
+    if (parseInt(paramId) !== parseInt(tokenUserId)) {
+      throw new CustomError("Unauthorized", 403);
+    }
     next();
   } catch (error) {
     next(error);
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,14 +1,27 @@
 import { Router } from "express";
-import { authMiddleware } from "../middlewares/auth-middleware.js";
+import {
+  authenticationMiddleware,
+  authorizationMiddleware,
+} from "../middlewares/auth-middlewares.js";
 import { userController } from "../controllers/user-controller.js";
 
 const router = Router();
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-router.put("/:id", authMiddleware, userController.update);
-router.delete("/:id", authMiddleware, userController.delete);
-router.get("/me", authMiddleware, userController.findMe);
-router.get("/", authMiddleware, userController.findAll);
-router.get("/:id", authMiddleware, userController.findById);
+router.put(
+  "/:id",
+  authenticationMiddleware,
+  authorizationMiddleware,
+  userController.update
+);
+router.delete(
+  "/:id",
+  authenticationMiddleware,
+  authorizationMiddleware,
+  userController.delete
+);
+router.get("/me", authenticationMiddleware, userController.findMe);
+router.get("/", authenticationMiddleware, userController.findAll);
+router.get("/:id", authenticationMiddleware, userController.findById);
 
 export default router;
