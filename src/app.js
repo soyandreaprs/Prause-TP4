63d9288da08d533fea0e89b32aa62f8955818481
Andrea <soyandreaprs@gmail.com>
2026-01-17 20:21:32 -0300

    Corrección de error en archivo principal

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import express from "express";
 import config from "./config/config.js";
 import { connection } from "./config/database.js";
 import { errorHandler } from "./middlewares/error-handler.js";
-import { info } from "./d"
+import { info } from "./docs/info.js";
 import router from "./routes/user-routes.js";
 
 const app = express();
 
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSDoc(info)))
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSDoc(info)));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,6 +22,6 @@ connection()
 
 const PORT = config.PORT;
 
-// app.listen(PORT, () => console.log(`Servidor funcionando en puerto ${PORT}`)); 
+// app.listen(PORT, () => console.log(`Servidor funcionando en puerto ${PORT}`));
 
 export default app;
